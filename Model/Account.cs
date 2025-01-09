285e4f0a26d50f8d557318cff87780bd2884e0b1
Tarik Kadric <kadric.tarik@hotmail.com>
2025-01-09 20:57:16 +0100

    Added Account Class

---
+++ Model/Account.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Banking_API.Model
+{
+    public class Account
+    {
+        [Key]
+        public int AccountID { get; set; }
+        [Required]
+        public int CustomerID { get; set; }
+        [Required]
+        public AccountTypeEnum AccountType { get; set; }
+        [Required]
+        public double Balance { get; set; }
+    }
+
+    public enum AccountTypeEnum
+    {
+        Girokonto,
+        Sparbuch,
+        Tagesgeldkonto
+    }
+}
