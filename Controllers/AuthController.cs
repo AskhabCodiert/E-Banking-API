6deda3ea3c60c984b210ecbcbb28e022ed274d2a
AskhabCodiert <ashab.dzhamulaev@gmail.com>
2025-01-11 00:22:59 +0100

    commented

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,64 +1,64 @@
-﻿using E_Banking_API.Model;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
-using System.Text;
+﻿//using E_Banking_API.Model;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using System.Security.Claims;
+//using System.Text;
 
 
-namespace E_Banking_API.Controllers
-{
-    [ApiController]
-    [Route("api/[controller]")]
-    public class AuthController : ControllerBase
-    {
-        private readonly IConfiguration _config;
+//namespace E_Banking_API.Controllers
+//{
+//    [ApiController]
+//    [Route("api/[controller]")]
+//    public class AuthController : ControllerBase
+//    {
+//        private readonly IConfiguration _config;
 
-        public AuthController(IConfiguration config)
-        {
-            _config = config;
-        }
+//        public AuthController(IConfiguration config)
+//        {
+//            _config = config;
+//        }
 
-        //[HttpPost("login")]
-        //public IActionResult Login([FromBody] LoginDto loginDto)
-        //{
-        //    //here the username and password are checked
-        //    var user = GetUserFromDatabase(loginDto.Username);
-        //    if (user == null || !VerifyPassword(user.PasswordHash, loginDto.Password))
-        //    {
-        //        return Unauthorized("Invalid credentials");
-        //    }
+//        //[HttpPost("login")]
+//        //public IActionResult Login([FromBody] LoginDto loginDto)
+//        //{
+//        //    //here the username and password are checked
+//        //    var user = GetUserFromDatabase(loginDto.Username);
+//        //    if (user == null || !VerifyPassword(user.PasswordHash, loginDto.Password))
+//        //    {
+//        //        return Unauthorized("Invalid credentials");
+//        //    }
 
-        //    // jwt token is created
-        //    var token = GenerateJwtToken(user);
-        //    return Ok(new { token });
-        //}
+//        //    // jwt token is created
+//        //    var token = GenerateJwtToken(user);
+//        //    return Ok(new { token });
+//        //}
 
-        public IActionResult Login([FromBody] Customer customer)
-        {
+//        public IActionResult Login([FromBody] Customer customer)
+//        {
             
-        }
+//        }
 
 
 
-        private string GenerateJwtToken(User user)
-        {
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var claims = new[]
-            {
-            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-            new Claim(ClaimTypes.Name, user.Username)
-        };
+//        private string GenerateJwtToken(User user)
+//        {
+//            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+//            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+//            var claims = new[]
+//            {
+//            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+//            new Claim(ClaimTypes.Name, user.Username)
+//        };
 
-            var token = new JwtSecurityToken(
-                _config["Jwt:Issuer"],
-                _config["Jwt:Issuer"],
-                claims,
-                expires: DateTime.Now.AddHours(1),
-                signingCredentials: creds);
+//            var token = new JwtSecurityToken(
+//                _config["Jwt:Issuer"],
+//                _config["Jwt:Issuer"],
+//                claims,
+//                expires: DateTime.Now.AddHours(1),
+//                signingCredentials: creds);
 
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
-    }
+//            return new JwtSecurityTokenHandler().WriteToken(token);
+//        }
+//    }
 
-}
+//}
